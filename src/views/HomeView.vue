418e319c3c9775975853f5f8e7c6426fa40ba850
Erkezh <erke.zhanabaieva@gmail.com>
2026-05-07 17:44:21 +0500

    refactor: remove placeholder instruction text from HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,9 +7,6 @@ import HeroSection from '@/components/HeroSection.vue'
   <main class="w-full min-h-screen">
     <AppHeader />
     <HeroSection />
-    
-    <div class="text-center text-gray-400 mt-20">
-      <p>Ожидаю ваших следующих инструкций...</p>
-    </div>
+
   </main>
 </template>
